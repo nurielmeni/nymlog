5181ede004b61fe5894eee968b87ef6bc7c1cef0
Meni Nuriel <meni@menis-mac-mini.mynet>
2019-11-23 00:00:17 +0200

    s

--- routes/logger.js
+++ routes/logger.js
@@ -8,6 +8,7 @@ router.get('/', function(req, res, next) {
   
 /* POST log message. */
 router.post('/', function(req, res, next) {
+    console.log('Route logger /:', req);
     if (typeof req.body.logs !== 'undefined' && req.body.logs.length > 0) {
         console.log(req.body.logs[0]);
         res.send(`Log Recieved: ${JSON.stringify(req.body.logs[0])}`);
