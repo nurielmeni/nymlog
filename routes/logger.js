85c1a025bcf7bf18dd030916934038553cbdd91c
Meni Nuriel <meni@menis-mac-mini.mynet>
2019-11-22 23:40:46 +0200

    ss

--- routes/logger.js
+++ routes/logger.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
     if (typeof req.body.logs !== 'undefined' && req.body.logs.length > 0) {
         console.log(req.body.logs[0]);
-        res.send(`Log Recieved: ${req.body.logs[0]}`);
+        res.send(`Log Recieved: ${JSON.stringify(req.body.logs[0])}`);
     } else {
         res.send('Log Errro: no log recieved');
     }
