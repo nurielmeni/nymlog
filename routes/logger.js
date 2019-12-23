ce35240c227b21bc757506f0ea934f2671984029
Meni Nuriel <nurielmeni@gmail.com>
2019-12-23 11:06:24 +0200

    se

--- routes/logger.js
+++ routes/logger.js
@@ -12,18 +12,19 @@ router.get('/', function(req, res, next) {
 /* POST log message. */
 router.post('/', async (req, res, next) => {
     if (typeof req.body.logs !== 'undefined' && req.body.logs.length > 0) {
-        console.log(req.body.logs[0]);
+        //console.log(req.body.logs[0]);
         const log = new Log(req.body.logs[0]);
-        console.log('Log',log);
-        console.log('UpdateConsole: ', www.updateConsole);
+
+        //console.log('Log',log);
+        //console.log('UpdateConsole: ', www.updateConsole);
 
         try {
             const newLog = await log.save();
-            console.log(www.updateConsole());
+            //console.log(www.updateConsole());
 
             www.updateConsole(log);
 
-            res.send(JSON.stringify(log));
+            //res.send(JSON.stringify(log));
             res.end();
         } catch (err) {
             res.status(400).json({ message: err.message });
