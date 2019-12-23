afc922a7830550e629c17708e56602918da90e0e
Meni Nuriel <nurielmeni@gmail.com>
2019-12-23 15:50:09 +0200

    ds

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,6 @@ const bcrypt = require('bcryptjs');
 const { User, validate, validateLogin } = require('../models/user.js');
 const express = require('express');
 const router = express.Router();
-const config = require('config');
-
 
 router.get('/current', auth, async (req, res) => {
   const user = await User.findById(req.user._id).select('-password');
@@ -25,7 +23,7 @@ router.post('/', async (req, res) => {
     password: req.body.password,
     email: req.body.email
   });
-  user.password = await bcrypt.hash(user.password, config.get('myprivatekey'));
+  user.password = bcrypt.hashSync(user.password, 10);
   await user.save();
 
   const token = user.generateAuthToken();
@@ -44,17 +42,16 @@ router.post('/login', async (req, res) => {
   //find an existing user
   let user = await User.findOne({ email: req.body.email });
   console.log(user);
-  let password = await bcrypt.compare(user.password, config.get('myprivatekey'), (err, res) => {
-    console.log('err, res: ', err, res);
-    if (!err) {
-      const token = user.generateAuthToken();
-      res.header('x-auth-token', token).send({
-        _id: user._id,
-        name: user.name,
-        email: user.email
-      });
-    }
-  });
+  let valid = bcrypt.compareSync(req.body.password, user.password);
+  if (valid) {
+    const token = user.generateAuthToken();
+    res.header('x-auth-token', token).send({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    });
+  }
+
   console.log('Password: ', password);
   return res.status(400).send('Could not authenticate: email or password not valid.');
 });
