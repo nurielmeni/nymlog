3b4e1ac5eec61a3bad320b57358b2248afa93c70
Meni Nuriel <nurielmeni@gmail.com>
2019-12-23 16:23:58 +0200

    d

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ module.exports = function(req, res, next) {
   //get the token from the header if present
   const token = req.headers['x-access-token'] || req.headers['authorization'];
   //if no token found, return response (without going to the next middelware)
-  if (!token) return res.status(401).send('Access denied. No token provided.');
+  if (!token) return res.status(401).redirect('/login');
 
   try {
     //if can verify the token, set req.user and pass to next middleware
@@ -14,6 +14,6 @@ module.exports = function(req, res, next) {
     next();
   } catch (ex) {
     //if invalid token
-    res.status(400).redirect('/login')
+    res.status(400).redirect('/login');
   }
 };
