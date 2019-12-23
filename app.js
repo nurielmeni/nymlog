95c3ba0bf144346cc1f34ce643576b7f2f9c2445
Meni Nuriel <meni@menis-mac-mini.mynet>
2019-12-24 00:34:36 +0200

    f

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 //Import the mongoose module
 const mongoose = require('mongoose');
@@ -31,6 +32,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
