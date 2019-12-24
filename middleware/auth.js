5611c83b62ec8f60ff3aa51754835427a36561b2
Meni Nuriel <meni@menis-mac-mini.mynet>
2019-12-24 03:16:08 +0200

    s

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,9 +3,9 @@ const config = require('config');
 
 module.exports = function(req, res, next) {
   //get the token from the header if present
-  const token = req.headers['x-access-token'] || req.headers['authorization'];
+  const token = req.headers['x-access-token'] || req.headers['authorization'] || req.signedCookies['access_token'];
   console.log('Token: ', token);
-  
+
   //if no token found, return response (without going to the next middelware)
   if (!token) return res.status(401).redirect('/login');
 
