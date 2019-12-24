5f969fbfcc1f03e347047e6cf2b4fbbbcd6aa84a
Meni Nuriel <meni@menis-mac-mini.mynet>
2019-12-24 03:11:21 +0200

    f

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 //app.use(bodyParser.json());
+app.use(cookieParser('NYMedia cookie secret'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
