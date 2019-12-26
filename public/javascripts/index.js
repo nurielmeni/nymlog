8b4acde78dde67485873da94c6b1cfcff9bd02f1
Meni Nuriel <meni@Menis-Mac-mini.local>
2019-12-26 12:41:26 +0200

    s

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,17 +1,17 @@
 const logger = log.noConflict();
-const sender = remote.apply(logger, {format:remote.json});
+const sender = remote.apply(logger, { format: remote.json });
 const socket = io();
-const nymConsole = document.getElementById('nym-console');
+const nymConsole = document.getElementById("nym-console");
 
 logger.enableAll();
 
-const getLogEntry = (json) => {
-    if (!json) return;
+const getLogEntry = json => {
+  if (!json) return;
 
-    const logEntry = document.createElement('div');
-    logEntry.id = json._id;
-    logEntry.className = 'nym-log';
-    logEntry.innerHTML = `
+  const logEntry = document.createElement("div");
+  logEntry.id = json._id;
+  logEntry.className = "nym-log";
+  logEntry.innerHTML = `
         <p class="summary">
             <span class="timestamp">${json.timestamp}</span>
             <span class="logger">${json.logger}</span>
@@ -22,19 +22,19 @@ const getLogEntry = (json) => {
         <p class="stacktrace" hidden>${json.stacktrace}</p>
     `;
 
-    logEntry.addEventListener('click', function() {
-        toggleVisibility(this.querySelector('p.stacktrace'));
-    });
-    return logEntry;
+  logEntry.addEventListener("click", function() {
+    toggleVisibility(this.querySelector("p.stacktrace"));
+  });
+  return logEntry;
 };
 
-const toggleVisibility = (elm) => {
-    elm.hidden = !elm.hidden;
-}
+const toggleVisibility = elm => {
+  elm.hidden = !elm.hidden;
+};
 
-socket.on('update console', (json) => {
-    if (json) {
-        console.log('UPDATE Console: ', json);
-        nymConsole.appendChild(getLogEntry(json));
-    }
+socket.on("updateConsole", json => {
+  if (json) {
+    console.log("UPDATE Console: ", json);
+    nymConsole.appendChild(getLogEntry(json));
+  }
 });
