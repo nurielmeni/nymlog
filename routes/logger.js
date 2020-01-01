637cb938af4a71a3f542778c9b500dfe18339181
Meni Nuriel <nurielmeni@gmail.com>
2020-01-01 15:44:28 +0200

    n

--- routes/logger.js
+++ routes/logger.js
@@ -1,37 +1,37 @@
-const www = require('../bin/www');
-const express = require('express');
-const Log = require('../models/log');
+const www = require("../bin/www");
+const express = require("express");
+const Log = require("../models/log");
 const router = express.Router();
 
 /* GET log message. */
-router.get('/', function(req, res, next) {
-    res.send("Recieved: " + req);
-    res.end();
+router.get("/", function(req, res, next) {
+  res.send("Recieved: " + req);
+  res.end();
 });
-  
+
 /* POST log message. */
-router.post('/', async (req, res, next) => {
-    if (typeof req.body.logs !== 'undefined' && req.body.logs.length > 0) {
-        //console.log(req.body.logs[0]);
-        const log = new Log(req.body.logs[0]);
+router.post("/", async (req, res, next) => {
+  if (typeof req.body.logs !== "undefined" && req.body.logs.length > 0) {
+    //console.log(req.body.logs[0]);
+    const log = new Log(req.body.logs[0]);
 
-        //console.log('Log',log);
-        //console.log('UpdateConsole: ', www.updateConsole);
+    //console.log('Log',log);
+    //console.log('UpdateConsole: ', www.updateConsole);
 
-        try {
-            const newLog = await log.save();
-            //console.log(www.updateConsole());
+    try {
+      const newLog = await log.save();
+      //console.log(www.updateConsole());
 
-            www.updateConsole(log);
+      www.updateConsole(log);
 
-            //res.send(JSON.stringify(log));
-            res.end();
-        } catch (err) {
-            res.status(400).json({ message: err.message });
-        }
-    } else {
-        res.status(400).json({ message: 'Log Errro: no log recieved'});
+      //res.send(JSON.stringify(log));
+      res.end();
+    } catch (err) {
+      res.status(400).json({ message: err.message });
     }
+  } else {
+    res.status(400).json({ message: "Log Errro: no log recieved" });
+  }
 });
-  
+
 module.exports = router;
