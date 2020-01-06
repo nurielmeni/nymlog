768c847c3405814a2ffd5d1dcdd85c25ecf28b97
Meni Nuriel <nurielmeni@gmail.com>
2020-01-06 16:11:51 +0200

    d

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const bodyParser = require("body-parser");
 
 //Import the mongoose module
 const mongoose = require("mongoose");
@@ -32,7 +31,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(logger("dev"));
-app.use(bodyParser.text());
+app.use(express.text());
 app.use(cookieParser("NYMedia cookie secret"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
