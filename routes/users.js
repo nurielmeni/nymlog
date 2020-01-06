c12e011dde0c73493af5bfc1ce0b76f58c0ae6d7
Meni Nuriel <nurielmeni@gmail.com>
2020-01-06 14:15:40 +0200

    x

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,22 @@
-const auth = require('../middleware/auth');
-const bcrypt = require('bcryptjs');
-const { User, validate, validateLogin } = require('../models/user.js');
-const express = require('express');
+const auth = require("../middleware/auth");
+const bcrypt = require("bcryptjs");
+const { User, validate, validateLogin } = require("../models/user.js");
+const express = require("express");
 const router = express.Router();
 
-router.get('/current', auth, async (req, res) => {
-  const user = await User.findById(req.user._id).select('-password');
+router.get("/current", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
   res.send(user);
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   // validate the request body first
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   //find an existing user
   let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send('User already registered.');
+  if (user) return res.status(400).send("User already registered.");
 
   user = new User({
     name: req.body.name,
@@ -27,15 +27,16 @@ router.post('/', async (req, res) => {
   await user.save();
 
   const token = user.generateAuthToken();
-  res.header('x-auth-token', token).send({
+  res.header("x-auth-token", token).send({
     _id: user._id,
     name: user.name,
     email: user.email
   });
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   // validate the request body first
+  console.log(req.body);
   const { error } = validateLogin(req.body);
   if (error) {
     res.statusMessage = error.details[0].message;
@@ -45,23 +46,23 @@ router.post('/login', async (req, res) => {
   //find an existing user
   let user = await User.findOne({ email: req.body.email });
   if (!user) {
-    res.statusMessage = 'Could not authenticate: email or password not valid.';
+    res.statusMessage = "Could not authenticate: email or password not valid.";
     return res.status(400).end();
   }
   console.log(user);
-  console.log('Pass compare: ', req.body.password, user.password);
-  
+  console.log("Pass compare: ", req.body.password, user.password);
+
   let valid = bcrypt.compareSync(req.body.password, user.password);
   if (valid) {
     const token = user.generateAuthToken();
-    res.cookie('access_token', token, { signed: true , maxAge: 300000 });
-    res.header('x-auth-token', token).json({
+    res.cookie("access_token", token, { signed: true, maxAge: 300000 });
+    res.header("x-auth-token", token).json({
       _id: user._id,
       name: user.name,
       email: user.email
     });
   } else {
-    res.statusMessage = 'Could not authenticate: email or password not valid.';
+    res.statusMessage = "Could not authenticate: email or password not valid.";
     return res.status(400).end();
   }
 });
