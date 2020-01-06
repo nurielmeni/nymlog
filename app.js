b92870db2b240db6a9648e640db34bb85d314a81
Meni Nuriel <nurielmeni@gmail.com>
2020-01-06 13:55:57 +0200

    d

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 //const bodyParser = require('body-parser');
 
 //Import the mongoose module
-const mongoose = require('mongoose');
-const mongoUsername = process.env.MONGO_USERNAME || 'admin';
-const mongoPassword = process.env.MONGO_PASSWORD || 'admin';
-const mongoServer = process.env.MONGO_SERVER || 'mongo';
-const mongoPort = process.env.MONGO_PORT || '27017';
-const mongoDatabase = process.env.MONGO_DATABASE || 'nymlog';
+const mongoose = require("mongoose");
+const mongoUsername = process.env.MONGO_USERNAME || "admin";
+const mongoPassword = process.env.MONGO_PASSWORD || "admin";
+const mongoServer = process.env.MONGO_SERVER || "mongo";
+const mongoPort = process.env.MONGO_PORT || "27017";
+const mongoDatabase = process.env.MONGO_DATABASE || "nymlog";
 //Set up default mongoose connection
 //const mongoDB = 'mongodb+srv://nurielmeni:Meni1971@cluster0-0isxr.mongodb.net/test?retryWrites=true&w=majority';
 const mongoDB = `mongodb://${mongoUsername}:${mongoPassword}@${mongoServer}:${mongoPort}/${mongoDatabase}`;
@@ -19,29 +20,30 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 //Get the default connection
 const db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const loggerRouter = require('./routes/logger');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const loggerRouter = require("./routes/logger");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.options("*", cors());
+app.use(logger("dev"));
 //app.use(bodyParser.json());
-app.use(cookieParser('NYMedia cookie secret'));
+app.use(cookieParser("NYMedia cookie secret"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/logger', loggerRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/logger", loggerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -52,11 +54,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', { error: err });
+  res.render("error", { error: err });
 });
 
 module.exports = app;
