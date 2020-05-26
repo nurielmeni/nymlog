7b45ec5605171a34303614c35b14562bb3d867b9
Meni Nuriel <nurielmeni@gmail.com>
2020-05-26 16:13:35 +0300

    Adding Front

---
+++ public/precache-manifest.f612c1e5a470509f95967b07cd2caab1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b6d66625b6a737f50f7",
+    "url": "/css/app.0ef55738.css"
+  },
+  {
+    "revision": "0d7c739ff13a402e127f",
+    "url": "/css/chunk-vendors.96a655cf.css"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/favicon.default.ico"
+  },
+  {
+    "revision": "abd74a085c5ba0610cdb64138a0896a3",
+    "url": "/img/nymLogger.ico"
+  },
+  {
+    "revision": "e75ceeae0fc40a80454b1e607a51a381",
+    "url": "/img/nymLogger.png"
+  },
+  {
+    "revision": "bb715a4a25d958050f6a8745c5e7ab1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b6d66625b6a737f50f7",
+    "url": "/js/app.8b356f49.js"
+  },
+  {
+    "revision": "0d7c739ff13a402e127f",
+    "url": "/js/chunk-vendors.a48547b6.js"
+  },
+  {
+    "revision": "8fdea3674fa614888734bbbd4c44ea23",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
